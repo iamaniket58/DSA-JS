da2d177be745ea244617ff49dff133d7dc5df9f2
iamaniket58 <iamaniket58@gmail.com>
2025-08-07 01:18:37 +0530

    Priority Queue- 1046. Last Stone Weight

---
+++ DSA-Saini/Heaps/1046_LastStoneWeight.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+
+//Bruite Force approach-Sort an array, pop last 2 elements and do
+var lastStoneWeight = function (stones) {
+    while (stones.length > 1) {
+        stones.sort((a, b) => a - b);
+        let y = stones.pop();
+        let x = stones.pop();
+        if (y > x) {
+            stones.push(y - x)
+        }
+    }
+    return stones[0]
+
+};
+
+//using priority Queue
+var lastStoneWeight = function (stones) {
+    let maxHeap = new MaxPriorityQueue();
+    for (let i = 0; i < stones.length; i++) {
+        maxHeap.enqueue(stones[i]);
+    }
+    while (maxHeap.size() > 1) {
+        let y = maxHeap.dequeue();
+        let x = maxHeap.dequeue();
+        if (y > x) {
+            maxHeap.enqueue(y - x)
+        }
+    }
+    return maxHeap.front() ?? 0;
+
+};
