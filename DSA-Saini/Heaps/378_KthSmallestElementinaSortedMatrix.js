73090852bdc90e19cf23289748a4d3bb4325111e
iamaniket58 <iamaniket58@gmail.com>
2025-08-08 02:47:31 +0530

    Kth Smallest Index Optimization

--- DSA-Saini/Heaps/378_KthSmallestElementinaSortedMatrix.js
+++ DSA-Saini/Heaps/378_KthSmallestElementinaSortedMatrix.js
@@ -3,20 +3,52 @@
  * @param {number} k
  * @return {number}
  */
-var kthSmallest = function(matrix, k) {
-    let minHeap=new MinPriorityQueue(x=>x.val);
-    let n=matrix[0].length;
-    for(let i=0;i<matrix.length;i++){
-        minHeap.push({val:matrix[i][0],row:i,col:0})
+var kthSmallest = function (matrix, k) {
+    let minHeap = new MinPriorityQueue(x => x.val);
+    let n = matrix[0].length;
+    for (let i = 0; i < matrix.length; i++) {
+        minHeap.push({ val: matrix[i][0], row: i, col: 0 })
     }
-    count=0
-    while(count<k-1){
-        let poped=minHeap.pop();
-        if(poped.col+1<n){
-            minHeap.push({val:matrix[poped.row][poped.col+1],row:poped.row,col:poped.col+1})
+    count = 0
+    while (count < k - 1) {
+        let poped = minHeap.pop();
+        if (poped.col + 1 < n) {
+            minHeap.push({ val: matrix[poped.row][poped.col + 1], row: poped.row, col: poped.col + 1 })
         }
         count++
-        
+
+    }
+    return minHeap.pop().val;
+};
+//More Cleaner way
+var kthSmallest = function (matrix, k) {
+    let minHeap = new MinPriorityQueue(x => x.val);
+    let n = matrix[0].length;
+    for (let i = 0; i < matrix.length; i++) {
+        minHeap.push({ val: matrix[i][0], row: i, col: 0 })
+    }
+    for (let count = 0; count < k - 1; count++) {
+        let { val, row, col } = minHeap.pop();
+        if (col + 1 < n) {
+            minHeap.push({ val: matrix[row][col + 1], row: row, col: col + 1 })
+        }
+    }
+    return minHeap.pop().val;
+};
+
+
+//Min(n,k)- reduce time complexity
+var kthSmallest = function (matrix, k) {
+    let minHeap = new MinPriorityQueue(x => x.val);
+    let n = matrix[0].length;
+    for (let i = 0; i < Math.min(n,k); i++) {
+        minHeap.push({ val: matrix[i][0], row: i, col: 0 })
+    }
+    for (let count = 0; count < k - 1; count++) {
+        let { val, row, col } = minHeap.pop();
+        if (col + 1 < n) {
+            minHeap.push({ val: matrix[row][col + 1], row: row, col: col + 1 })
+        }
     }
     return minHeap.pop().val;
 };
