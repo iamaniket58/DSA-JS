09f163e55da2189af4270dc436cae2b472d5df20
iamaniket58 <iamaniket58@gmail.com>
2025-08-10 18:27:31 +0530

    Optimized- Remove Duplicates from Sorted List II

--- DSA-Saini/LinkedList0/_82_RemoveDuplicatesfromSortedListII.js
+++ DSA-Saini/LinkedList0/_82_RemoveDuplicatesfromSortedListII.js
@@ -9,38 +9,61 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
+//This is my Method
+var deleteDuplicates = function (head) {
 
     //First Remove all the dupliactes- and store the value which we deleted in Set
-    if(head==null)return null;
-    let prev=head;
-    let curr=head.next;
-    let set=new Set();
-    while(curr){
-        if(curr.val==prev.val){
-            prev.next=curr.next;
+    if (head == null) return null;
+    let prev = head;
+    let curr = head.next;
+    let set = new Set();
+    while (curr) {
+        if (curr.val == prev.val) {
+            prev.next = curr.next;
             set.add(prev.val)
-            curr=prev.next
+            curr = prev.next
         }
-        else{
-            prev=prev.next;
-            curr=curr.next;
+        else {
+            prev = prev.next;
+            curr = curr.next;
         }
     }
 
     //Now delete the single duplicate Node
-    prev=head;
-    curr=head.next;
-    while(curr){
-        if(set.has(curr.val)){
-            prev.next=prev.next.next;
-            curr=prev.next
+    prev = head;
+    curr = head.next;
+    while (curr) {
+        if (set.has(curr.val)) {
+            prev.next = prev.next.next;
+            curr = prev.next
         }
-        else{
-            prev=prev.next;
-            curr=curr.next
+        else {
+            prev = prev.next;
+            curr = curr.next
         }
     }
-    if(set.has(head.val))return head.next
+    if (set.has(head.val)) return head.next
     return head;
+};
+
+//Optimzed One
+var deleteDuplicates = function (head) {
+    let sentinel = new ListNode(0, head);
+    let prev = sentinel;
+    while (head != null) {
+        if (head.next && head.val == head.next.val) {
+            // Skip all duplicates
+            while (head.next && head.val == head.next.val) {
+                head = head.next
+            }
+            // Remove all occurrences
+            head = head.next;
+            prev.next = head
+        }
+        else {
+            prev = prev.next;
+            head = head.next;
+        }
+    }
+    return sentinel.next
 };
