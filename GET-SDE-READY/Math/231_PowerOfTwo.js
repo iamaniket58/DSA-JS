6b8c98ace7a00afcd415b61336944130bf2f0aea
iamaniket58 <iamaniket58@gmail.com>
2025-08-16 18:02:29 +0530

    power of two using Bitwise

--- GET-SDE-READY/Math/231_PowerOfTwo.js
+++ GET-SDE-READY/Math/231_PowerOfTwo.js
@@ -2,7 +2,7 @@ var isPowerOfTwo = function (n) {
     if (n == 1) return true
     while (n >= 1) {
         if (n == 1) return true
-        n = n / 2   
+        n = n / 2
     }
     return false;
 
@@ -15,4 +15,10 @@ var isPowerOfTwo = function (n) {
         n = Math.floor(n / 2)
     }
     return n == 1
+};
+
+//using Bitwise AND
+var isPowerOfTwo = function (n) {
+    if (n <= 0) return false
+    return (n & (n - 1)) == 0;
 };
