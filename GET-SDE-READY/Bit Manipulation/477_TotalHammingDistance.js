1ee977b9a2e80a0fdbee12f55fb9d37c0056bda6
iamaniket58 <iamaniket58@gmail.com>
2025-08-22 00:46:38 +0530

    Total Hamming Distance

--- GET-SDE-READY/Bit Manipulation/477_TotalHammingDistance.js
+++ GET-SDE-READY/Bit Manipulation/477_TotalHammingDistance.js
@@ -5,36 +5,55 @@
 
 
 
+//Optimized
+var totalHammingDistance = function (nums) {
+    let ans = 0;
+    for (let i = 0; i < 32; i++) {
+        let ones = 0;
+        let zeros = 0;
+        for (let num of nums) {
+            if (num & (1 << i)) {
+                ones++;
+            }
+            else {
+                zeros++;
+            }
 
-//Bruite Force
-var totalHammingDistance = function(nums) {
-    let ans=0;
-    for(let i=0;i<nums.length-1;i++){
-        for(let j=i+1;j<nums.length;j++){
-            let xor=nums[i]^nums[j];
-            let setBit=countSetBitsByConvertingToBinary(xor)
-            ans=ans+ setBit
+        }
+        ans = ans + (ones * zeros)
+    }
+    return ans;
+};
+
+//Bruite Force- Time Limit Exceeds
+var totalHammingDistance = function (nums) {
+    let ans = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            let xor = nums[i] ^ nums[j];
+            let setBit = countSetBitsByConvertingToBinary(xor)
+            ans = ans + setBit
         }
     }
     return ans
 };
 
 //Counting digit using (check if i'th bit is set) 
-function numberOfSetBits(num){
-    let count=0;
-    for(let i=0;i<32;i++){
-        if(num & (1<<i))count++
+function numberOfSetBits(num) {
+    let count = 0;
+    for (let i = 0; i < 32; i++) {
+        if (num & (1 << i)) count++
     }
     return count;
 }
 
 //COunting didgit using Binary conversion
-function countSetBitsByConvertingToBinary(num){
-    let count=0
-    while(num>0){
-        let rem=num%2;
-        if(rem==1)count++
-        num=Math.floor(num/2);
+function countSetBitsByConvertingToBinary(num) {
+    let count = 0
+    while (num > 0) {
+        let rem = num % 2;
+        if (rem == 1) count++
+        num = Math.floor(num / 2);
     }
     return count
 
