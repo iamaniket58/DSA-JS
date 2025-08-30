0b37bd8cee3300658e767182a5d6c536d35074f9
iamaniket58 <iamaniket58@gmail.com>
2025-08-31 02:21:35 +0530

    Search Insert Position- Binary Search

---
+++ GET-SDE-READY/Searching-Beginer/35_SearchInsertPosition.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+//Using Binary Search
+var searchInsert = function(nums, target) {
+    let l=0;
+    let r=nums.length-1;
+    while(l<=r){
+        let mid=Math.floor(l+(r-l)/2);
+        if(nums[mid]==target)return mid
+        if(target>nums[mid]){
+            l=mid+1
+        }
+        else{
+            r=mid-1
+        }
+    }
+    return l;
+};
+
+//Using Linear Search
+var searchInsert = function(nums, target) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==target)return i;
+        else if(nums[i]>target)return i;
+    }
+    return nums.length;
+};
