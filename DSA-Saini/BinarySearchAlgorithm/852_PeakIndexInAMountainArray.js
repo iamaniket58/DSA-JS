c99d60503f41bfcd402f2a824149c1b357f58471
iamaniket58 <iamaniket58@gmail.com>
2025-08-31 17:03:18 +0530

    Peak Index in a Mountain Array

--- DSA-Saini/BinarySearchAlgorithm/852_PeakIndexInAMountainArray.js
+++ DSA-Saini/BinarySearchAlgorithm/852_PeakIndexInAMountainArray.js
@@ -1,26 +1,39 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+//using Binary Search
+var peakIndexInMountainArray = function (arr) {
 
+    let l = 0;
+    let r = arr.length - 1;
+    while (l < r) {
+        let mid = l + Math.floor((r - l) / 2);
+        if (arr[mid + 1] > arr[mid]) {
+            l = mid + 1
+        }
+        else {
+            r = mid
+        }
+    }
+    return l
+}
+
+
+//Linear Search
 var peakIndexInMountainArray = function(arr) {
-    let maxIndex=0;
-    for(let i=1;i<arr.length;i++){
-        if(arr[i]>arr[maxIndex]){
-            maxIndex=i
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>arr[i+1])return i
+    }
+};
+
+
+var peakIndexInMountainArray = function (arr) {
+    let maxIndex = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i
         }
     }
     return maxIndex;
 };
-
-//using Binary Search
-var peakIndexInMountainArray = function(arr) {
-
-    let l=0;
-    let r=arr.length-1;
-    while(l<r){
-        let mid=l+Math.floor((r-l)/2);
-        if(arr[mid+1]>arr[mid]){
-            l=mid+1
-        }
-        else{
-            r=mid
-        }
-    }
-    return l}
