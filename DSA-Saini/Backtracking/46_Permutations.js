a3582d870173109bcf868a4b58545b0f558a6014
iamaniket58 <iamaniket58@gmail.com>
2025-09-30 01:57:04 +0530

    Permutation

--- DSA-Saini/Backtracking/46_Permutations.js
+++ DSA-Saini/Backtracking/46_Permutations.js
@@ -20,3 +20,21 @@ var permute = function (nums) {
     backtrack([])
     return result
 };
+
+//This is my method which i inspired by Permutation II Solution, Here i m checking choices array
+var permute = function(nums) {
+    let result=[];
+    let backtrack=(path,choices)=>{
+        if(path.length==nums.length){
+            result.push([...path]);
+            return;
+        }
+        for(let i=0;i<choices.length;i++){
+            path.push(choices[i]);
+            backtrack(path,[...choices.slice(0,i),...choices.slice(i+1)]);
+            path.pop()
+        }
+    }
+    backtrack([],nums);
+    return result;
+};
--- DSA-Saini/Backtracking/47_PermutationsII.js
+++ DSA-Saini/Backtracking/47_PermutationsII.js
@@ -21,3 +21,29 @@ var permuteUnique = function (nums) {
     backtracking([],nums);
     return result;
 };
+
+//GPT Solution
+var permuteUnique = function(nums) {
+    nums.sort((a, b) => a - b);
+    let result = [];
+    let used = new Array(nums.length).fill(false);
+
+    let backtrack = (path) => {
+        if (path.length === nums.length) {
+            result.push([...path]);
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue; // skip duplicates
+            path.push(nums[i]);
+            used[i] = true;
+            backtrack(path);
+            used[i] = false;
+            path.pop();
+        }
+    };
+
+    backtrack([]);
+    return result;
+};
