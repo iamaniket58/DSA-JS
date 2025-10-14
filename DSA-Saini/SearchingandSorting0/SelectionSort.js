2b3c6d2e4d4393709af32d625598029c33511daf
iamaniket58 <iamaniket58@gmail.com>
2025-10-14 23:18:16 +0530

    Selection Sort

--- DSA-Saini/SearchingandSorting0/SelectionSort.js
+++ DSA-Saini/SearchingandSorting0/SelectionSort.js
@@ -12,7 +12,7 @@ function SelectionSort(array) {
     }
     return array
 }
-console.log(SelectionSort([7, 9, 8, 2, 5]));
+// console.log(SelectionSort([7, 9, 8, 2, 5]));
 
 function SelectionSort1(array) {
     for (let i = 0; i < array.length - 1; i++) { //-1 is optional here
@@ -31,4 +31,23 @@ function SelectionSort1(array) {
     }
     return array
 }
-console.log(SelectionSort1([7, 9, 8, 2, 5]));
+// console.log(SelectionSort1([7, 9, 8, 2, 5]));
+
+
+//This is not selection Sort because it swaps aloy inside the Loop
+function Sorting(arr){
+    let n=arr.length;
+    for(let i=0;i<n-1;i++){
+        let min=i;
+        for(let j=i+1;j<n;j++){
+            if(arr[j]<arr[min]){
+                let temp=arr[min];
+                arr[min]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    return arr;
+}
+// console.log(Selection_Sort([7,2,5,4,1]));
+console.log("Result",Selection_Sort([4, 2, 2, 1]));
