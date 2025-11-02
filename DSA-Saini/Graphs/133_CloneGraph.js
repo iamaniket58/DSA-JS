12789b9c8bfad057e786cdbd9a1e6ca72662a467
iamaniket58 <iamaniket58@gmail.com>
2025-11-02 15:13:53 +0530

    133 Clone Graph

---
+++ DSA-Saini/Graphs/133_CloneGraph.js
@@ -0,0 +1,33 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+var cloneGraph = function(root) {
+    if(!root)return null;
+    let q=[root];
+    let visited=new Map();
+    let clonedGraph=new Node(root.val);
+    visited.set(root,clonedGraph);
+
+    while(q.length>0){
+        let curr=q.shift();
+        for(let n of curr.neighbors){
+            if(!visited.has(n)){
+                q.push(n);
+                visited.set(n,new Node(n.val))
+            }
+            let cloneCurr=visited.get(curr);
+            cloneCurr.neighbors.push(visited.get(n));
+        }
+    }
+    return clonedGraph;
+};
+
