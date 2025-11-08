5c54656930eabf93c9892b3b09d35d22edb03173
iamaniket58 <iamaniket58@gmail.com>
2025-11-08 20:58:46 +0530

    Graph: Detect Cycle in Undirected Connected Graph

---
+++ DSA-Saini/Graphs/DetectCycleInUndirectedConnectedGraph.js
@@ -0,0 +1,28 @@
+function HasCycle(edges) {
+    let graph = {};
+    for (let [x, y] of edges) {
+        if (!graph[x]) graph[x] = [];
+        if (!graph[y]) graph[y] = [];
+        graph[x].push(y);
+        graph[y].push(x);
+    }
+
+    let visited = new Set();
+    let dfs = (curr, parent) => {
+        visited.add(curr);
+        for (let n of graph[curr]) {
+            if (!visited.has(n)) {
+                // return dfs(n, curr); //Sir's Solution is wrong
+                if (dfs(n, curr)) return true;  // ✅ continue exploring
+            }
+            else if (n != parent) { //else if( visited.has(n) && n!=parent)
+                return true;
+            }
+        }
+        return false;
+    }
+    return dfs(0, -1);
+}
+console.log(HasCycle([[0,1],[1,2],[2,0]]))
+console.log(HasCycle([[0,1],[1,2],[2,3]]))
+console.log(HasCycle([[0,1],[1,2],[2,3],[3,4],[1,4]]))
