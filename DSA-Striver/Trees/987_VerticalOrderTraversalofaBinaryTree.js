6c1c18578dfb0c3ed7277d0e1c2f763882022b10
iamaniket58 <iamaniket58@gmail.com>
2026-02-06 18:36:11 +0530

    Vertical Order Traversal of a Binary Tree

--- DSA-Striver/Trees/987_VerticalOrderTraversalofaBinaryTree.js
+++ DSA-Striver/Trees/987_VerticalOrderTraversalofaBinaryTree.js
@@ -10,6 +10,35 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+//Iteration-Optimized
+var verticalTraversal = function (root) {
+    let map = {};
+    let minHd = Infinity;
+    let maxHd = -Infinity;
+    let ans = [];
+    let q = [[root, 0, 0]];
+    while (q.length) {
+        let [curr, hd, level] = q.shift();
+        if (!map[hd]) map[hd] = [];
+        map[hd].push([curr.val, level]);
+
+        minHd=Math.min(minHd,hd);
+        maxHd=Math.max(maxHd,hd);
+        
+        curr.left && q.push([curr.left, hd - 1, level + 1]);
+        curr.right && q.push([curr.right, hd + 1, level + 1]);
+    }
+    for (let i = minHd; i <= maxHd; i++) {
+        map[i].sort((a, b) => {
+            if (a[1] == b[1]) return a[0] - b[0]
+            return a[1] - b[1];
+        });
+        ans.push(map[i].map(x => x[0]));
+    }
+    return ans;
+};
+
+//iterative
 var verticalTraversal = function (root) {
     let q = [[root, 0, 0]];
     let ans = [];
@@ -30,6 +59,34 @@ var verticalTraversal = function (root) {
             return a[1]-b[1]
         })
         ans.push(obj[key].map(x=>x[0]));
+    }
+    return ans;
+};
+
+//Recursive
+var verticalTraversal = function (root) {
+    let map = {};
+    let minHd = Infinity;
+    let maxHd = -Infinity;
+    let ans=[];
+    let traverse = (curr, level, hd) => {
+        if (!curr) return;
+        minHd = Math.min(minHd, hd);
+        maxHd = Math.max(maxHd, hd);
+
+        if (!map[hd]) map[hd] = [];
+        map[hd].push([curr.val, level]);
+
+        traverse(curr.left, level + 1, hd - 1);
+        traverse(curr.right, level + 1, hd + 1);
+    }
+    traverse(root, 0, 0);
+    for (let i = minHd; i <= maxHd; i++) {
+        map[i].sort((a,b)=>{
+            if(a[1]==b[1])return a[0]-b[0]
+            return a[1]-b[1];
+        });
+        ans.push(map[i].map(x=>x[0]));
     }
     return ans;
 };
