a513a25e6e5571d93a9904a114f88f0e8fd74b66
iamaniket58 <iamaniket58@gmail.com>
2026-02-15 00:00:07 +0530

    Shortest Path in Unweighted Graph

--- DSA-Saini/Graphs/ShortestPathinUnweightedGraphBFS.js
+++ DSA-Saini/Graphs/ShortestPathinUnweightedGraphBFS.js
@@ -16,8 +16,8 @@ function ShortestDistance(graph, src) {
     }
     return distance;
 }
-const graph=[
-    [1,2],
+const graph = [
+    [1, 2],
     [3],
     [4],
     [5],
@@ -26,31 +26,41 @@ const graph=[
 ];
 // console.log(ShortestDistance(graph,0))
 
-//Find Path of the shortest distance
+//Find Path of the shortest distance between source to Destination
 function ShortestDistancePath(graph, src, destination) {
     let n = graph.length;
     let distance = new Array(n).fill(Infinity);
-    distance[src] = 0;
-
+    let parent = new Array(n).fill(-1);
+    distance[src] = 0
     let q = [src];
     while (q.length) {
         let curr = q.shift();
+        if (curr == destination) break;
         for (let neighbor of graph[curr]) {
             if (distance[neighbor] == Infinity) {
                 distance[neighbor] = distance[curr] + 1;
+                parent[neighbor] = curr;
                 q.push(neighbor);
             }
         }
-
     }
-    return distance;
+    // If destination unreachable
+    if (distance[destination] === Infinity) return [];
+
+    let path=[];
+    let current=destination;
+    while(current!=-1){
+        path.push(current);
+        current=parent[current];
+    }
+    return path.reverse();
 }
-const graph1=[
-    [1,2],
+const graph1 = [
+    [1, 2],
     [3],
     [4],
     [5],
     [3],
     [],
 ];
-console.log(ShortestDistancePath(graph1,0,5))
+console.log(ShortestDistancePath(graph1, 0, 5))
